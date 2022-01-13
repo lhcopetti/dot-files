805836ee01b2c64c620fb2bb5c10570c75f7256e
Luís Copetti <lhcopetti@gmail.com>
2022-01-13 19:25:24 -0300

    Add sanity check for the result of the dot-files path lookup

--- install-dot-files.sh
+++ install-dot-files.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 scriptPath="$0"
-dotFilesPath="$(dirname $scriptPath)"
 echo "Running script: $scriptPath"
+
+if [ "$(uname)" == "Linux" ]; then
+	dotFilesPath="$(readlink -f $scriptPath | xargs dirname)"
+else
+    dotFilesPath="$(dirname $scriptPath)"
+fi
+
 echo "dot-files path is at: $dotFilesPath"
+
+if [ "$dotFilesPath" == "." ]; then
+	echo "This is probably an error. Double check the output dot-files path because it should contain the full path to the dot-files directory from root /"
+	exit 1;
+fi
 
 echo "Changing location to HOME directory"
 pushd ~
